002c2833ca8ebfcd1a4de8eeb51aff07340b349c
duong <duong.bui@opswaat.com>
2022-09-21 13:35:54 +0700

    1st commit

---
+++ Data Structures And Algorithms/maximum-length-of-repeated-subarray.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findLength = function (nums1, nums2) {
+  //check leng nums1 vs nums2
+  //use arr with longer leng, if equal use nums1
+  //for loop
+};
+
+console.log(findLength([[1, 2, 3, 2, 1]], [3, 2, 1, 4, 7]));
